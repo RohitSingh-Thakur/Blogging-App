a56e8165901299afe4dc449a80c2c2b209e2c157
RohitSingh <thakurrohitdsingh@gmail.com>
2023-07-18 15:51:40 +0530

    initial

---
+++ src/main/java/com/singh/rdst/BlogingAppApplication.java
@@ -0,0 +1,13 @@
+package com.singh.rdst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogingAppApplication { // hellp
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogingAppApplication.class, args);
+	}
+
+}
